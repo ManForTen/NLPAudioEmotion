23a5b4d7958c7c71b02169e03e7354819d7cdc11
Artem Petrikov <manforten@tandex.ru>
2023-12-14 00:53:55 +0300

    new updates

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from st_audiorec import st_audiorec
-import speech_recognition as sr
-import numpy as np
-import librosa.display
-import matplotlib.pyplot as plt
+import torch
+import torchaudio
+from transformers import pipeline
 
 st.write("""
 # Лабораторная работа 6
@@ -19,41 +18,25 @@ if uploaded_file:
     st.write("Файл успешно загружен!")
 
     # Преобразование байтов в аудиофайл
-    y, sr = librosa.load(uploaded_file, sr=None)
+    y, sr = torchaudio.load(uploaded_file, normalize=True)
 
     st.audio(uploaded_file, format='audio/wav')
 
     # Построение графика временного сигнала (waveplot)
     st.subheader("Waveplot:")
-    fig_wave, ax_wave = plt.subplots(figsize=(12, 4))
-    ax_wave.plot(np.linspace(0, len(y) / sr, len(y)), y)
-    ax_wave.set_title('Waveplot')
-    ax_wave.set_xlabel('Время (сек.)')
-    ax_wave.set_ylabel('Amplitude')
-    st.pyplot(fig_wave)
+    st.line_chart(y[0].numpy())
 
-    # Построение спектрограммы
-    st.subheader("Spectrogram:")
-    fig_spec, ax_spec = plt.subplots(figsize=(10, 4))
-    img = librosa.display.specshow(librosa.amplitude_to_db(librosa.stft(y), ref=np.max), y_axis='log', x_axis='time',
-                                   ax=ax_spec, cmap='viridis')
-    plt.colorbar(img, format='%+2.0f dB')
-    ax_spec.set_title('Spectrogram')
-    st.pyplot(fig_spec)
-
-    # Определение эмоции с использованием speech_recognition
+    # Определение эмоции с использованием transformers
     st.subheader("Эмоция:")
 
-    recognizer = sr.Recognizer()
-    audio_file = sr.AudioFile(uploaded_file)
+    # Load pre-trained emotion classification model
+    classifier = pipeline('audio-classification', model='maksimekin/emotion-audio')
 
-    with audio_file as source:
-        audio_data = recognizer.record(source)
+    # Convert audio tensor to numpy array
+    audio_np = y[0].numpy()
 
-    try:
-        emotion = recognizer.recognize_google(audio_data, language='ru-RU')
-        st.write("Определенная эмоция: ", emotion)
-    except sr.UnknownValueError:
-        st.write("Не удалось определить эмоцию")
-    except sr.RequestError as e:
-        st.write("Ошибка сервиса распознавания речи; {0}".format(e))
+    # Classify emotion
+    result = classifier(audio_np)
+
+    # Display the result
+    st.write(result)
