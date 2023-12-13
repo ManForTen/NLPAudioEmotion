e5ca4662855e873055a1148d4124b7180db10cec
Artem Petrikov <manforten@tandex.ru>
2023-12-13 21:03:29 +0300

    Third commit ver17

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ wav_audio_data = st_audiorec()
 
 uploaded_files = st.file_uploader("Загрузите записанный голос", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    # Чтение аудиофайла
+    audio_tensor, sample_rate = torchaudio.load(uploaded_file)
+
+    # Проверка, что аудио моно (1 канал)
+    if audio_tensor.shape[0] == 2:
+        audio_tensor = audio_tensor.mean(dim=0, keepdim=True)
+
+    # Применение преобразований, например, вычисление спектрограммы
+    transform = T.MelSpectrogram(sample_rate=sample_rate, n_mels=128)
+    mel_spectrogram = transform(audio_tensor)
+
+    # Вывод спектрограммы
+    st.write(f"Спектрограмма для файла: {uploaded_file.name}")
+    st.image(mel_spectrogram[0].numpy(), caption='Мел-спектрограмма', use_column_width=True)
