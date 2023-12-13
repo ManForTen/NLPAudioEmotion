64234d5d3ccc8641114753e5f0d8236bdb7dd81d
Artem Petrikov <manforten@tandex.ru>
2023-12-13 19:56:23 +0300

    Third commit ver8

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import streamlit as st
 from st_audiorec import st_audiorec
+import numpy as np
+import librosa.display
+import matplotlib.pyplot as plt
 
 
 st.write("""
 # Лабораторная работа 6
 Запишите свой голос
 """)
+
+# Ваш код для записи аудио
 wav_audio_data = st_audiorec()
+
+# Проверка, что запись прошла успешно
+if wav_audio_data is not None:
+    # Преобразование аудио в одномерный массив
+    audio_array = np.squeeze(wav_audio_data)
+
+    # Создание спектрограммы с использованием librosa
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(audio_array)), ref=np.max)
+
+    # Отображение спектрограммы с использованием matplotlib
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(D, sr=44100, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    plt.show()
+
+    # Отображение спектрограммы в Streamlit
+    st.pyplot()
+
+
 
 
 
