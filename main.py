a0c19b5150bdb5202b5f0f0afc92173bcdee33a6
Artem Petrikov <manforten@tandex.ru>
2023-12-13 20:11:33 +0300

    Third commit ver14

--- main.py
+++ main.py
@@ -12,26 +12,7 @@ st.write("""
 
 wav_audio_data = st_audiorec()
 
-# Проверка, что запись прошла успешно
-if wav_audio_data is not None:
-    # Преобразование аудио в одномерный массив с плавающей точкой
-    audio_array = np.squeeze(wav_audio_data)
-
-    # Нормализация аудио, учитывая возможность деления на 0
-    max_abs_value = np.max(np.abs(audio_array))
-
-    if max_abs_value != 0:
-        audio_array = audio_array / max_abs_value
-
-    # Создание спектрограммы с использованием librosa
-    D = librosa.amplitude_to_db(np.abs(librosa.stft(audio_array)), ref=np.max)
-
-    # Отображение спектрограммы с использованием matplotlib
-    plt.figure(figsize=(10, 4))
-    librosa.display.specshow(D, sr=44100, x_axis='time', y_axis='log')
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Spectrogram')
-    st.pyplot()
+st.write("Содержимое переменной `wav_audio_data`:", wav_audio_data)
 
 
 
