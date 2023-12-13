6102888d43196cecfd89ec63286d814386cd4f8a
Artem Petrikov <manforten@tandex.ru>
2023-12-13 19:43:31 +0300

    Third commit ver7

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# This is a sample Python script.
 import streamlit as st
 from st_audiorec import st_audiorec
 
 
 st.write("""
-# My first app
-Hello *world!*
+# Лабораторная работа 6
+Запишите свой голос
 """)
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
+
+print(wav_audio_data)
 
