77213ee1e9f5b3cb24addf8185a57bc8d05371ba
Artem Petrikov <manforten@tandex.ru>
2023-12-13 19:48:33 +0300

    Third commit ver7

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
-a = st.audio(wav_audio_data, format='audio/wav')
+a = str(wav_audio_data)
 st.write(a)
 
 
