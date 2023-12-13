7a9604e9ed125afafdc485112698f64dc4581a25
Artem Petrikov <manforten@tandex.ru>
2023-12-13 17:08:47 +0300

    Third commit

--- main.py
+++ main.py
@@ -2,38 +2,15 @@
 import streamlit as st
 import sounddevice as sd
 import numpy as np
+from st_audiorec import st_audiorec
 
 st.write("""
 # My first app
 Hello *world!*
 """)
 
+wav_audio_data = st_audiorec()
 
-def record_audio(duration=5, sample_rate=44100):
-    st.text("Идет запись...")
-
-    # Запись аудио
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
-    sd.wait()
-
-    st.text("Запись завершена!")
-
-    return audio_data
-
-def main():
-    st.title("Запись голоса с помощью Streamlit")
-
-    duration = st.slider("Выберите длительность записи (в секундах)", min_value=1, max_value=10, value=5)
-
-    if st.button("Начать запись"):
-        audio_data = record_audio(duration)
-
-        # Визуализация аудиоданных
-        st.line_chart(np.squeeze(audio_data))
-
-        # Сохранение аудио
-        st.audio(audio_data, format="audio/wav", start_time=0)
-
-if __name__ == "__main__":
-    main()
+if wav_audio_data is not None:
+    st.audio(wav_audio_data, format='audio/wav')
 
