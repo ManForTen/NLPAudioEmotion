1e9787a5a4634d0f98d30f4898b17bda0872427c
Artem Petrikov <manforten@tandex.ru>
2023-12-13 20:30:36 +0300

    Third commit ver17

--- main.py
+++ main.py
@@ -17,11 +17,17 @@ wav_audio_data = st_audiorec()
 if wav_audio_data is not None:
     audio = whisper.load_audio(wav_audio_data)
     audio = whisper.pad_or_trim(audio)
+
+    # Используем конструктор WhisperASR
+    model = whisper.WhisperASR(model_size="medium")
+
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
     _, probs = model.detect_language(mel)
     st.write(f"Язык аудио: {max(probs, key=probs.get)}")
+
     options = whisper.DecodingOptions(fp16=False)
     result = whisper.decode(model, mel, options)
+
     st.write("Текст:", result.text)
 
 
